0f4bf6f2db741191e2b3eb4f414a5f0f3e6d588c
Colin Rundel <rundel@gmail.com>
2018-09-06 09:49:55 -0400

    Fix

--- hw1_whitelist.R
+++ hw1_whitelist.R
@@ -3,18 +3,18 @@ allowed_files = c("hw1.Rmd",
                   "wercker.yml",
                   "hw1.Rproj",
                   "hw1_whitelist.R",
+                  "fizzbuzz.png",
                   ".gitignore")
 
 files = dir()
 disallowed_files = files[!(files %in% allowed_files)]
 
-if (length(disallowed_files != 0))
-{
+if (length(disallowed_files != 0)) {
   cat("Disallowed files found:\n")
   cat("  (remove the following files from your repo)\n\n")
 
-  for(file in disallowed_files)
-    cat("*",file,"\n")
+  for (file in disallowed_files)
+    cat("*", file, "\n")
 
-  quit("no",1,FALSE)
+  quit("no", 1, FALSE)
 }
